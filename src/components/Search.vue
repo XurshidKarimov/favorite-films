8fc92e4952e35e3b993d6cbe4f42062f1783784f
unknown <kaxudi34801@gmail.com>
2023-04-23 08:26:01 +0500

    vuetify input for search add

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,12 +1,24 @@
 <template>
      <div class="search">Search</div>
+     <v-form>
+          <v-text-field
+          v-model="movieName"
+          required
+          class="movieInput"
+          ></v-text-field>
+     </v-form>
 </template>
 
 <script setup>
+     import {ref} from 'vue';
+
+     const movieName = ref('');
 
 </script>
 
 
-<style lang="scss" scoped>
-
+<style>
+     .v-text-field input{
+          padding: 10px !important;
+     }
 </style>
